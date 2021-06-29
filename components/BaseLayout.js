93b053925c85aa7ede0698351946dd1170a6c5a3
Renato Pozzi <r.pozzi@bluecube.it>
2021-06-30 01:11:17 +0200

    added tailwindcss & markdown css & layout

--- components/BaseLayout.js
+++ components/BaseLayout.js
@@ -1,13 +1,16 @@
 import { useRouter } from "next/router";
+import { Navbar } from "./Navbar";
 
 export const BaseLayout = ({ frontMatter, children, ...rest }) => {
   const router = useRouter();
   const isHome = router.pathname === "/";
 
   return (
-    <>
-      Helo
-      {children}
-    </>
+    <div className="flex flex-col">
+      <Navbar />
+      <div className="flex flex-1 justify-center p-8 sm:p-16">
+        <main className="prose dark:prose-dark max-w-prose">{children}</main>
+      </div>
+    </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
+import { ThemeProvider } from "next-themes";
 import { AppWrapper } from "../components/AppWrapper";
 
 import "../styles/globals.css";
 
 const MyApp = ({ Component, pageProps, ...rest }) => {
   return (
-    <AppWrapper {...rest}>
-      <Component {...pageProps} />
-    </AppWrapper>
+    <ThemeProvider attribute="class">
+      <AppWrapper {...rest}>
+        <Component {...pageProps} />
+      </AppWrapper>
+    </ThemeProvider>
   );
 };
 
