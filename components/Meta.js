06b95308855db5161ec609edf0ab9cd4a66c5489
Renato Pozzi <r.pozzi@bluecube.it>
2021-07-01 22:09:39 +0200

    added icons and logo

--- components/Meta.js
+++ components/Meta.js
@@ -3,7 +3,30 @@ import Head from "next/head";
 export const Meta = () => {
   return (
     <Head>
-      <link rel="icon" type="image/svg+xml" href="" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/icons/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/icons/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/icons/favicon-16x16.png"
+      />
+      <link
+        rel="mask-icon"
+        href="/icons/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
       <meta property="og:image" content="/images/og_image.png" />
       <meta property="og:type" content="website" />
       <meta property="og:site_name" content="Stacks Docs" />
