083ab592f3518d9cbd3d6b0fbbb88a0743352d8b
Renato Pozzi <r.pozzi@bluecube.it>
2021-07-01 22:45:51 +0200

    commented navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -47,6 +47,7 @@ export const Navbar = () => {
                   </NextLink>
                 </div>
               </div>
+              {/*
               <div className="flex-1 flex justify-center px-2 lg:ml-6 lg:justify-end">
                 <div className="max-w-lg w-full lg:max-w-xs">
                   <label htmlFor="search" className="sr-only">
@@ -69,6 +70,7 @@ export const Navbar = () => {
                   </div>
                 </div>
               </div>
+              */}
               <div className="flex lg:hidden">
                 {/* Mobile menu button */}
                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
